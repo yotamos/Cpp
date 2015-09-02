3181dd59911ac3bab2b058ef76f73587a38f30c7
yotamos <yotam.mos@gatech.edu>
2015-09-01 22:11:00 -0400

    Create world.h

---
+++ world.h
@@ -0,0 +1,14 @@
+#ifndef world_H
+#define world_H
+
+#include <iostream>
+#include <string>
+
+#include "Person.h"
+#include "Address.h"
+#include "Major.h"
+#include "Building.h"
+#include "School.h"
+#include "Student.h"
+
+#endif
